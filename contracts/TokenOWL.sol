c01fb3b357d613f66f4b79e8d7113e46374923d4
josojo <josojo@hotmail.de>
2018-02-18 19:02:31 +0100

    updated the tests

--- contracts/TokenOWL.sol
+++ contracts/TokenOWL.sol
@@ -87,13 +87,13 @@ contract TokenOWL is ProxiedMaster, StandardToken {
     }
 
     /// @dev Burns OWL.
-    /// @param address Address of OWL owner
+    /// @param user Address of OWL owner
     /// @param amount Amount of OWL to be burnt
     function burnOWL(address user, uint amount)
         public
     {
-        allowances[user][msg.sender].sub(amount);
-        balances[user].sub(amount);
+        allowances[user][msg.sender] = allowances[user][msg.sender].sub(amount);
+        balances[user] = balances[user].sub(amount);
         totalTokens = totalTokens.sub(amount);
         Burnt(msg.sender, user, amount);
     }
