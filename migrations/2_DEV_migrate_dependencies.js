15330b6a096b6385c0e824d77b9166d7a9d1cc40
Angel Rodriguez <anxolin@gmail.com>
2018-12-03 23:47:07 +0100

    User migrations from truffle4

--- migrations/2_DEV_migrate_dependencies.js
+++ migrations/2_DEV_migrate_dependencies.js
@@ -1,7 +1,7 @@
 /* global artifacts, web3 */
 /* eslint no-undef: "error" */
 
-const migrateDependencies = require('../src/migrations-truffle-4/2_DEV_migrate_dependencies')
+const migrateDependencies = require('../src/migrations-truffle-5/2_DEV_migrate_dependencies')
 
 module.exports = function (deployer, network, accounts) {
   return migrateDependencies({
