9489714f443f1f487f3c9b610389354b467e4e80
Daniel Sánchez <daniel.sanchez@gnosis.pm>
2018-12-05 13:51:50 +0100

    Truffle 5 migration: Do not deploy GNO Token contract if already deployed in DEV

--- src/migrations-truffle-5/2_DEV_migrate_dependencies.js
+++ src/migrations-truffle-5/2_DEV_migrate_dependencies.js
@@ -2,6 +2,19 @@ async function migrate ({ artifacts, deployer, network, accounts, web3 }) {
   const deployUtils = require('@gnosis.pm/util-contracts/src/migrations-truffle-5')
   const deployGno = require('@gnosis.pm/gno-token/src/migrations-truffle-5')
 
+  const TokenGNO = artifacts.require('TokenGNO')
+
+  function getGnoAddress () {
+    try {
+      return TokenGNO.address
+    } catch (error) {
+      // return TokenGNO.address throw an error if there's no config address
+      // for this network
+      return null
+    }
+  }
+  const gnoAddress = getGnoAddress()
+
   if (network === 'development') {
     const deployParams = {
       artifacts,
@@ -13,7 +26,11 @@ async function migrate ({ artifacts, deployer, network, accounts, web3 }) {
     }
 
     await deployUtils(deployParams)
-    await deployGno(deployParams)
+    if (!gnoAddress) {
+      await deployGno(deployParams)
+    } else {
+      console.log(`No need to deploy GNO Token contract. Using: ${gnoAddress}`)
+    }
   } else {
     console.log('Not in development, so nothing to do. Current network is %s', network)
   }
