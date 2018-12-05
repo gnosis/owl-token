a2077d62e271c13c8b193a62e59dcd626f1fd3b0
Daniel Sánchez <daniel.sanchez@gnosis.pm>
2018-12-05 13:38:35 +0100

    Do not deploy GNO Token in dev network if already deployed

--- src/migrations-truffle-4/2_DEV_migrate_dependencies.js
+++ src/migrations-truffle-4/2_DEV_migrate_dependencies.js
@@ -2,6 +2,19 @@ function migrate ({ artifacts, deployer, network, accounts, web3 }) {
   const deployMath = require('@gnosis.pm/util-contracts/src/migrations-truffle-4')
   const deployGno = require('@gnosis.pm/gno-token/src/migrations-truffle-4')
 
+  const TokenGNO = artifacts.require('TokenGNO')
+
+  function getGnoAddress () {
+    try {
+      return TokenGNO.address
+    } catch (error) {
+      // return TokenGNO.address throw an error if there's no config address
+      // for this network
+      return null
+    }
+  }
+  const gnoAddress = getGnoAddress()
+
   if (network === 'development') {
     const deployParams = {
       artifacts,
@@ -13,7 +26,13 @@ function migrate ({ artifacts, deployer, network, accounts, web3 }) {
     }
     deployer
       .then(() => deployMath(deployParams))
-      .then(() => deployGno(deployParams))
+      .then(() => {
+        if (!gnoAddress) {
+          deployGno(deployParams)
+        } else {
+          console.log(`No need to deploy GNO contract. Using: ${gnoAddress}`)
+        }
+      })
   } else {
     console.log('Not in development, so nothing to do. Current network is %s', network)
   }
