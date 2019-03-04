d5e09365ca4f8482a85834af68f7863ca13b017a
Velenir <velenir@gmail.com>
2019-03-04 19:05:53 +0300

    throw error when no LOCK_END time provided

--- migrations/4_deploy_OWL_airdrop.js
+++ migrations/4_deploy_OWL_airdrop.js
@@ -7,6 +7,12 @@ let gnoLockEndTime
 // i.e. LOCK_END_TIME='2018-06-12T16:00:00+02:00'
 if (process.env.LOCK_END_TIME) {
   gnoLockEndTime = new Date(Date.parse(process.env.LOCK_END_TIME))
+} else {
+  throw new Error(`
+  LOCK_END_TIME environment variable is no specified
+  It must be in ISO date format, e.g:
+  LOCK_END_TIME='2019-06-12T16:00:00+02:00' npm run migrate
+  `)
 }
 
 module.exports = function (deployer, network, accounts) {
