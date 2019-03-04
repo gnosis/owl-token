e48a18d075d20ea7291874e4958427b3ac699718
Velenir <velenir@gmail.com>
2019-03-04 17:06:07 +0300

    fix linter errors

--- test/test_proxy.js
+++ test/test_proxy.js
@@ -1,3 +1,5 @@
+/* global artifacts, contract, assert */
+
 const { time } = require('openzeppelin-test-helpers')
 const { assertRejects } = require('./utils.js')
 const TokenOWL = artifacts.require('TokenOWL')
@@ -66,7 +68,7 @@ contract('TokenOWL - Proxy', accounts => {
   it('creator can update masterCopy after time limit', async () => {
     await assertIsCreator(master)
     await tokenOWL.setMinter(minter)
-    const ans = await tokenOWL.updateMasterCopy({ from: master })
+    await tokenOWL.updateMasterCopy({ from: master })
     tokenOWL = await TokenOWLUpdateFixture.at(TokenOWLProxy.address)
 
     // testing that old variables are still available
