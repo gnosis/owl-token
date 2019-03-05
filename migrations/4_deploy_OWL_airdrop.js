ebe95d65d6ebe2fe697fd2ffe2e8813da6dd3382
Velenir <velenir@gmail.com>
2019-03-05 13:21:30 +0300

    fix typo

--- migrations/4_deploy_OWL_airdrop.js
+++ migrations/4_deploy_OWL_airdrop.js
@@ -4,12 +4,12 @@
 const deployAirdrop = require('../src/migrations-truffle-5/4_deploy_OWL_airdrop')
 
 let gnoLockEndTime
-// i.e. LOCK_END_TIME='2018-06-12T16:00:00+02:00'
+// i.e. LOCK_END_TIME='2019-06-12T16:00:00+02:00'
 if (process.env.LOCK_END_TIME) {
   gnoLockEndTime = new Date(Date.parse(process.env.LOCK_END_TIME))
 } else {
   throw new Error(`
-  LOCK_END_TIME environment variable is no specified
+  LOCK_END_TIME environment variable is not specified
   It must be in ISO date format, e.g:
   LOCK_END_TIME='2019-06-12T16:00:00+02:00' npm run migrate
   `)
