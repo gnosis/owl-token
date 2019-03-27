0e025021828981274491c22fee98ff73bd66200e
Velenir <velenir@gmail.com>
2019-03-27 13:06:29 +0300

    opt out of redeploying OWL on mainnet

--- migrations/3_deploy_OWL.js
+++ migrations/3_deploy_OWL.js
@@ -2,22 +2,20 @@
 /* eslint no-undef: "error" */
 
 const deployOwl = require('../src/migrations-truffle-5/3_deploy_OWL')
-const TokenOWL = artifacts.require('TokenOWL')
-const TokenOWLProxy = artifacts.require('TokenOWLProxy')
+const canSetMinterCheck = require('../src/utils/can_set_minter')
 
 module.exports = async function (deployer, network, accounts) {
-  let canSetMinter = false
-  try {
-    const tokenOWL = await TokenOWL.at(TokenOWLProxy.address)
-    const creator = await tokenOWL.creator()
-
-    canSetMinter = accounts[0].toLowerCase() === creator.toLowerCase()
-  } catch (error) {
-
-  }
+  const canSetMinter = await canSetMinterCheck({ artifacts, accounts })
 
   if (canSetMinter) {
     console.log('Can setMinter on already available TokenOWL. No need to redeploy')
+    return
+  }
+
+  // mainnet-fork is mainnet in --dry-run
+  if (!canSetMinter && (network === 'mainnet' || network === 'mainnet-fork')) {
+    console.log('Deploying account can\'t change TokenOWL minter')
+    console.log('As network is mainnet, change TokenOWL minter separately afterwards')
     return
   }
 
