1f9bb3f49693c17f0e8703595e5abdff647b9557
Felix Leupold <felixleupold90@gmail.com>
2020-09-11 12:00:50 +0200

    always run tet

--- travscripts/BranchScript.sh
+++ travscripts/BranchScript.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH = "master" || $TRAVIS_BRANCH = "develop" ]]; then
-  echo " ==> Detected PRINCIPAL branch - compiling and testing contracts"
-  #jump back to root
-  cd $TRAVIS_BUILD_DIR
-  echo " ==> JUMPING LOCATIONS: NOW IN $TRAVIS_BUILD_DIR"
-  
-  # compiling contracts
-  echo " ==> COMPILING contracts"
-  npm run compile;
+echo " ==> Detected PRINCIPAL branch - compiling and testing contracts"
+#jump back to root
+cd $TRAVIS_BUILD_DIR
+echo " ==> JUMPING LOCATIONS: NOW IN $TRAVIS_BUILD_DIR"
 
-  ls build/contracts/
+# compiling contracts
+echo " ==> COMPILING contracts"
+npm run compile;
 
-  # running contracts tests
-  echo " ==> RUNNING test"
-  npm test;
-else
-  echo " ==> No execution for branches other than MASTER or DEVELOP"
-fi;
+ls build/contracts/
+
+# running contracts tests
+echo " ==> RUNNING test"
+npm test;
